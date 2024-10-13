fd1e63fdb54fda0c24cb94e2fb80aaf4f003a640
Himanshu Ramteke <himanshuramteke007@gmail.com>
2024-10-13 22:25:22 +0530

    basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-3xl text-center font-bold underline bg-green-600">
+    Timer App
+    </h1>
+    </>
+  )
+}
+
+export default App
